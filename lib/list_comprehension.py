286b890840cd3a0e30475d5b946b1fab8ce66a6d
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-05-17 14:34:38 +0300

    lab competed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    final_list = [n for n in num_list if n % 2 == 0]
+    return final_list
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
